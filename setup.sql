a19b679fa7caa3c6b93546e7de82bb8e1b2ef2e6
Logan Long <lol@microsoft.com>
2019-09-25 12:00:12 -0500

    Update setup.sql
    Switch to Transact-SQL for Azure SQL support

--- setup.sql
+++ setup.sql
@@ -1,15 +1,15 @@
 CREATE TABLE subjects (
-   id SERIAL PRIMARY KEY,
+   id int IDENTITY(1,1) PRIMARY KEY,
    author_id INTEGER NOT NULL,
    subject VARCHAR(50) NOT NULL,
    description VARCHAR(250) NOT NULL, 
-   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   created_at datetimeoffset(7) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at datetimeoffset(7) NOT NULL DEFAULT CURRENT_TIMESTAMP,
    subject_image VARCHAR(2084) DEFAULT 'default_subject.png' NOT NULL 
 );
 
 CREATE TABLE topics (
-   id SERIAL PRIMARY KEY,
+   id int IDENTITY(1,1) PRIMARY KEY,
    author_id INTEGER NOT NULL,
    topic VARCHAR(50) NOT NULL,
    description VARCHAR(250) NOT NULL, 
@@ -18,7 +18,7 @@ CREATE TABLE topics (
 );
 
 CREATE TABLE posts (
-   id SERIAL PRIMARY KEY,
+   id int IDENTITY(1,1) PRIMARY KEY,
    resource VARCHAR(100) NOT NULL,
    resource_url VARCHAR(2084) NOT NULL,
    author_id INTEGER NOT NULL, 
@@ -31,55 +31,55 @@ CREATE TABLE posts (
    upvote_count INTEGER DEFAULT 0,
    post_image VARCHAR(2084) DEFAULT 'Image.svg' NOT NULL,
    author_image VARCHAR(2084) DEFAULT 'Avatar.svg' NOT NULL,
-   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   created_at datetimeoffset(7) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at datetimeoffset(7) NOT NULL DEFAULT CURRENT_TIMESTAMP,
    topic_id INTEGER NOT NULL REFERENCES topics(id)
 );
 
 CREATE TABLE upvoteposts (
-   id SERIAL PRIMARY KEY,
+   id int IDENTITY(1,1) PRIMARY KEY,
    user_id INTEGER NOT NULL,
    post_id INTEGER NOT NULL REFERENCES posts(id),
    vote_choice INTEGER DEFAULT 0 NOT NULL 
 );
 
 CREATE TABLE reports (
-   id SERIAL PRIMARY KEY,
+   id int IDENTITY(1,1) PRIMARY KEY,
    author_id INTEGER,
    description VARCHAR(250) NOT NULL,
-   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   created_at datetimeoffset(7) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at datetimeoffset(7) NOT NULL DEFAULT CURRENT_TIMESTAMP,
    reported_post_id INTEGER,
    reported_content_extension VARCHAR(200),
-   resolved boolean DEFAULT FALSE,
+   resolved bit NOT NULL DEFAULT 0,
    resolved_by VARCHAR(100) DEFAULT 'unresolved' NOT NULL,
-   teacher_created boolean DEFAULT FALSE
+   teacher_created bit NOT NULL DEFAULT 0
 );
 
 CREATE TABLE users (
-   id SERIAL PRIMARY KEY,
+   id int IDENTITY(1,1) PRIMARY KEY,
    username varchar(80) NOT NULL UNIQUE,
    email varchar(320) NOT NULL UNIQUE,
    firstname varchar(80) NOT NULL,
    lastname varchar(80) NOT NULL,
-   password bytea NOT NULL,
+   password varbinary(max) NOT NULL,
    school varchar(80) NOT NULL,
    profile_image varchar(2084) DEFAULT 'default.png' NOT NULL,
-   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   is_active boolean DEFAULT TRUE,
-   is_teacher boolean DEFAULT FALSE,
-   is_staff boolean DEFAULT FALSE
+   created_at datetimeoffset(7) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at datetimeoffset(7) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   is_active bit NOT NULL DEFAULT 1,
+   is_teacher bit NOT NULL DEFAULT 0,
+   is_staff bit NOT NULL DEFAULT 0
 );
 
 CREATE TABLE subject_subscription (
-   id SERIAL PRIMARY KEY,
+   id int IDENTITY(1,1) PRIMARY KEY,
    user_id INTEGER NOT NULL REFERENCES users(id),
    subject_id INTEGER NOT NULL 
 );
 
 CREATE TABLE topic_subscription (
-   id SERIAL PRIMARY KEY,
+   id int IDENTITY(1,1) PRIMARY KEY,
    user_id INTEGER NOT NULL REFERENCES users(id),
    topic_id INTEGER NOT NULL
 );
